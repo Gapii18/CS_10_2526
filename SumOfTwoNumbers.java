9e77b4c4cad855adb7b8293fac92e60ede8b7fed
Gapii18 <e5gzambran@berchmans.edu.co>
2025-09-26 23:39:46 -0500

    Actualizar SumOfTwoNumbers.java

--- SumOfTwoNumbers.java
+++ SumOfTwoNumbers.java
@@ -5,41 +5,26 @@
 
 import java.util.Scanner;
 
-/**
- * This program prompts the user to enter two numbers
- * and then calculates and displays their sum.
- */
-public class SumOfTwoNumbers {
-
+class MinutesToYearsDays {
     public static void main(String[] args) {
-
-        // 1. Create a Scanner object to read user input.
-        // This is necessary for the program to interact with the keyboard.
+        
+        
         Scanner scanner = new Scanner(System.in);
-
-        // 2. Ask the user to enter the first number.
-        System.out.println("Please enter the first number:");
-
-        // 3. Read the first number entered by the user and store it
-        // in a 'double' type variable. We use 'double' to allow for
-        // numbers with decimals.
-        double number1 = scanner.nextDouble();
-
-        // 4. Ask the user to enter the second number.
-        System.out.println("Please enter the second number:");
-
-        // 5. Read the second number and store it in another variable.
-        double number2 = scanner.nextDouble();
-
-        // 6. Calculate the sum of the two numbers.
-        double sum = number1 + number2;
-
-        // 7. Display the sum to the user.
-        // A descriptive message is used to make the result clear.
-        System.out.println("The sum of " + number1 + " and " + number2 + " is: " + sum);
-
-        // 8. Close the Scanner object.
-        // This is a good practice to free up system resources.
+        
+        
+        System.out.println("Por favor, ingrese la cantidad de minutos:");
+        long minutes = scanner.nextLong(); 
+        
+        
+        long minutesInYear = 60 * 24 * 365;   
+        long years = minutes / minutesInYear;
+        long remainingMinutes = minutes % minutesInYear; 
+        long days = remainingMinutes / (60 * 24); 
+        
+    
+        System.out.println(minutes + " minutos son aproximadamente " + years + " años y " + days + " días.");
+        
+        
         scanner.close();
     }
 }
